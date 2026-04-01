4b98101bbfddbf6b299349c599a01302384dc228
Eshan <eshan.savla@ipa.fraunhofer.de>
2026-04-01 16:44:04 +0200

    update demo for ROSSDL test

---
+++ demo/test_ws/src/test_system/include/rossdl_simple_test/Consumer.hpp
@@ -0,0 +1,41 @@
+// Copyright 2023 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef ROSSDL_SIMPLE_TEST__CONSUMER_HPP_
+#define ROSSDL_SIMPLE_TEST__CONSUMER_HPP_
+
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "rossdl_simple_test/Nodes.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+namespace rossdl_simple_test
+{
+
+class Consumer : public ConsumerBase
+{
+public:
+  explicit Consumer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+protected:
+  virtual void image_in_callback(sensor_msgs::msg::Image::SharedPtr msg);
+  virtual void description_in_callback(std_msgs::msg::String::SharedPtr msg);
+};
+
+}  // namespace rossdl_simple_test
+
+#endif  // ROSSDL_SIMPLE_TEST__CONSUMER_HPP_
